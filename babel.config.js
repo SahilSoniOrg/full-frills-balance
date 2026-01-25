452d3d41e249a0976ae773f2403dcda58da3dca2
Sahil Soni <SscSPs@gmail.com>
2026-01-25 16:36:08 +0530

    feat: Add dark mode splash screen support and update project dependencies and build configurations.

--- babel.config.js
+++ babel.config.js
@@ -9,5 +9,15 @@ module.exports = function (api) {
       // MUST be last
       'react-native-reanimated/plugin',
     ],
+    overrides: [
+      {
+        test: /src\/data\/models\/.*\.ts$/,
+        plugins: [
+          ['@babel/plugin-transform-class-properties', { loose: true }],
+          ['@babel/plugin-transform-private-methods', { loose: true }],
+          ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+        ],
+      },
+    ],
   };
 };
